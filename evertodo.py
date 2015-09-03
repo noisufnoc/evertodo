0157d48f63e4d0231830d5533765563166338b44
Mike Walker <noisufnoc@gmail.com>
2015-09-03 15:26:09 -0400

    ramblings in TODO

--- evertodo.py
+++ evertodo.py
@@ -3,5 +3,8 @@
 __author__ = 'noisufnoc'
 
 # TODO: Do something awesome
+# TODO: Probably going to use geeknote
+# TODO: Probably going to need dropbox funcationality
+# TODO: Might want to email some sort of digest, unless other tool is better
 
 # fin.
