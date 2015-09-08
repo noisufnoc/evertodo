e8439e0d43b796966fe4622e3ac067b8c67e7d62
Mike Walker <noisufnoc@gmail.com>
2015-09-08 19:37:48 -0400

    ramble ramble ramble

--- evertodo.py
+++ evertodo.py
@@ -4,6 +4,7 @@ __author__ = 'noisufnoc'
 
 # TODO: Do something awesome
 # TODO: Probably going to use geeknote
+# TODO: geeknote has no python module
 # TODO: Probably going to need dropbox funcationality
 # TODO: Might want to email some sort of digest, unless other tool is better
 
